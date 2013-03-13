a83a6be0efb2ac87992b50c8c16003fbfe43db54
Yitong Zhou <yitongz@boston-cluster.lti.cs.cmu.edu>
2013-03-13 19:09:30 -0400

    condor change

--- condor/indexbuild/JobGenerate.java
+++ condor/indexbuild/JobGenerate.java
@@ -16,14 +16,14 @@ public class JobGenerate{
 			bw.newLine();
 			bw.write("executable = /bos/usr0/yitongz/indri-5.4/buildindex/IndriBuildIndex");
 			bw.newLine();
-			bw.write("arguments = -corpus.path=../../data/_indri_xml_topic/MB"+s+"\\ ");
-			bw.write(" -corpus.class=trectext -field.name=CLEANTWEET -index=../../data/_indri_inv/MB"+s+" -indexType=indri");
+			bw.write("arguments = -corpus.path=/bos/tmp13/yitongz/data/_indri_xml_topic/MB"+s);
+			bw.write(" -corpus.class=trectext -field.name=CLEANTWEET -index=/bos/tmp13/yitongz/data/_indri_inv/MB"+s+" -indexType=indri");
 			bw.newLine();
-			bw.write("output = /bos/usr0/yitongz/condor/indexbuild/MB"+s+".job.out");
+			bw.write("output = /bos/usr0/yitongz/microblog.filter/condor/indexbuild/MB"+s+".job.out");
 			bw.newLine();
-			bw.write("log = /bos/usr0/yitongz/condor/indexbuild/MB"+s+".job.log");
+			bw.write("log = /bos/usr0/yitongz/microblog.filter/condor/indexbuild/MB"+s+".job.log");
 			bw.newLine();
-			bw.write("error = /bos/usr0/yitongz/condor/indexbuild/MB"+s+".job.err");
+			bw.write("error = /bos/usr0/yitongz/microblog.filter/condor/indexbuild/MB"+s+".job.err");
 			bw.newLine();
 			bw.write("queue");
 			bw.newLine();
@@ -31,7 +31,21 @@ public class JobGenerate{
 
 			w.write("condor_submit MB"+s+".job");
 			w.newLine();
+            if (i%15==0){
+                w.write("sleep 12m");
+                w.newLine();
+            }
+
 		}
 		w.close();
+        w=new BufferedWriter(new FileWriter(new File("rename.sh")));
+        for (int i=2;i<=49;i++){
+            if (i%5==1)
+                continue;
+            String s=nf.format(i);
+            w.write("mv /bos/tmp13/yitongz/data/_indri_xml_topic/MB"+s+"\\  /bos/tmp13/yitongz/data/_indri_xml_topic/MB"+s);
+            w.newLine();
+        }
+        w.close();
 	}
 }
